84cb16c3a8c6fd0f89eab7b2049d852e7edfc82b
Philip Lilliefelth <lilliefelth@gmail.com>
2013-03-05 15:21:17 +0000

    latest working version with layout no devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ookibox::Application.config.secret_token = 'cb23b6b247ad87ef5d8414d508b3584a372af80a58a0f4abac87857e4071810bcb58df2ade5f0582b5baf1b9ce460d8b653909fd76c8ede97648ce79532208b4'
