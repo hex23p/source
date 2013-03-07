6a4e7243a662fb2b9a7218546fb3f278bf8bbba6
Philip Lilliefelth <lilliefelth@gmail.com>
2013-03-07 20:26:32 +0000

    latest source code with test pages
    full source code @ 7/3/13

--- db/migrate/20130305120408_create_statuses.rb
+++ db/migrate/20130305120408_create_statuses.rb
@@ -1,6 +1,6 @@
 class CreateStatuses < ActiveRecord::Migration
   def change
-    create_table :statuses do |t|
+      create_table :statuses do |t|
       t.string :name
       t.text :content
 
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ test "a user should enter a first name" do
+ 	user = User.name
+ 	assert !user.save
+ 	assert !user.errors[:first_name].empty?
+
+ end
+
+ test "a user should enter a last name" do
+ 	user = User.name
+ 	assert !user.save
+ 	assert !user.errors[:last_name].empty?
+
+ end
+
+ test "a user should enter a profile name" do
+ 	user = User.name
+ 	assert !user.save
+ 	assert !user.errors[:profile_name].empty?
+
+ end
+
+ test "a user should have a unique profile name" do
+ 	user = User.name
+ 	user.profile_name = "hex23p"
+ 	user.email_address = "hex23p@gmail.com"
+ 	user.first_name = "Philip"
+ 	user.last_name = "lilliefelth"
+ 	user.password = "pml101180"
+ 	user.password_confirmation = "pml101180"
+ 	assert !user.save
+ 	assert !user.errors[:profile_name].empty?
+
+ end
 end
