0f927a15f8b395cfe6fb6d5fc0e2eb632bd0e062
Philip Lilliefelth <lilliefelth@gmail.com>
2013-03-08 20:07:09 +0000

    added layout, flash messages and alerts, fixed bug with format user name, added new custom routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,13 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :profile_name, presence: true
-  validates :profile_name, uniqueness: true
+  validates :profile_name, presence: true,
+                           uniqueness: true,
+                           format: {
+                            with: /\A[a-zA-Z0-9-_]+\Z/,
+                            message: 'must be formatted correctly.'
+                           }
+
 
   
   has_many :statuses
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,36 +2,42 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
  test "a user should enter a first name" do
- 	user = User.name
+ 	user = User.new
  	assert !user.save
  	assert !user.errors[:first_name].empty?
 
  end
 
  test "a user should enter a last name" do
- 	user = User.name
+ 	user = User.new
  	assert !user.save
  	assert !user.errors[:last_name].empty?
 
  end
 
  test "a user should enter a profile name" do
- 	user = User.name
+ 	user = User.new
  	assert !user.save
  	assert !user.errors[:profile_name].empty?
 
  end
 
  test "a user should have a unique profile name" do
- 	user = User.name
- 	user.profile_name = "hex23p"
- 	user.email_address = "hex23p@gmail.com"
- 	user.first_name = "Philip"
- 	user.last_name = "lilliefelth"
- 	user.password = "pml101180"
- 	user.password_confirmation = "pml101180"
+ 	user = User.new
+ 	user.profile_name = users(:philip).profile_name
+ 	
  	assert !user.save
  	assert !user.errors[:profile_name].empty?
 
  end
+
+ test "a user should have a profile name without spaces" do
+	 user = User.new
+	 user.profile_name = "My Profile With Spaces"
+
+	 assert !user.save
+	 assert !user.errors[:profile_name].empty?
+	 assert user.errors[:profile_name].include?("must be formatted correctly.")
+ end
 end
+
